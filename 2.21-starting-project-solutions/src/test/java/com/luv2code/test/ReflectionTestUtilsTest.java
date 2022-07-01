b5605417d173ab63c65e410b2d1b587038d03de2
LoweKumar <lowekr00@gmail.com>
2022-07-02 00:43:46 +0530

    1st Commit

--- 2.21-starting-project-solutions/src/test/java/com/luv2code/test/ReflectionTestUtilsTest.java
+++ 2.21-starting-project-solutions/src/test/java/com/luv2code/test/ReflectionTestUtilsTest.java
@@ -18,37 +18,5 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(classes=MvcTestingExampleApplication.class)
 public class ReflectionTestUtilsTest {
 
-    @Autowired
-    ApplicationContext context;
 
-    @Autowired
-    CollegeStudent studentOne;
-
-    @Autowired
-    StudentGrades studentGrades;
-
-    @BeforeEach
-    public void studentBeforeEach() {
-        studentOne.setFirstname("Eric");
-        studentOne.setLastname("Roby");
-        studentOne.setEmailAddress("eric.roby@luv2code_school.com");
-        studentOne.setStudentGrades(studentGrades);
-
-        ReflectionTestUtils.setField(studentOne, "id", 1);
-        ReflectionTestUtils.setField(studentOne, "studentGrades",
-                new StudentGrades(new ArrayList<>(Arrays.asList(
-                        100.0, 85.0, 76.50, 91.75))));
-    }
-
-    @Test
-    public void getPrivateField() {
-        assertEquals(1, ReflectionTestUtils.getField(studentOne, "id"));
-    }
-
-    @Test
-    public void invokePrivateMethod() {
-        assertEquals("Eric 1",
-                ReflectionTestUtils.invokeMethod(studentOne, "getFirstNameAndId"),
-                "Fail private method not call");
-    }
 }
